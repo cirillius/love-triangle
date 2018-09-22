35afef96cf632aa2ad7d28e24a86a36a5cb44f70
Cirill Dayneko <cirillius@gmail.com>
2018-09-22 18:02:41 +0300

    reliz lpve triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var counter = 0;
+  for (var i = 0; i < preferences.length; i++) {
+    if (preferences[preferences[preferences[i]-1]-1]-1 === i) {
+      counter++
+    }  
+  }
+  return Math.floor(counter/3);
 };
